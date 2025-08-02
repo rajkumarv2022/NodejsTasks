06ff49a0138e12eb80781bc8b856197ecad9e472
rajkumarv2022 <rajkumar.v2022cse@sece.ac.in>
2025-08-02 15:47:41 +0530

    loading content from file to website

--- express/app.js
+++ express/app.js
@@ -1,5 +1,10 @@
 const express=require('express');
 const app=express();
+const path=require('path');
+
+const fs=require('fs');
+
+// const data=fs.readFileSync('./files/file2.txt');
 
 app.get('/', (req,res) => {
     res.send(`<h1>Home Page</h1>`);
@@ -7,6 +12,11 @@ app.get('/', (req,res) => {
 
 app.get('/about', (req,res) => {
     res.send(`<h1>About Page</h1>`);
+} )
+
+app.get('/fileread', (req,res) => {
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname,'./files/file2.txt'));
 } )
 
 app.all('*', (req,res) => {
